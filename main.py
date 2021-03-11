f6b219f37d09a2cef8487daff4d13d45652f5218
Ivan Alvarenga <ivan.neun@gmail.com>
2021-03-10 23:08:44 -0300

    feat: added code verification for all files

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import src.search_cids as sc
+import pandas as pd
 
 
 if __name__ == '__main__':
 
-    path = './data/codigos.csv'
+    codes_file_path = '/home/user4/Projects/ufrj/ic/sp_cid_gravidez/data/codigos.csv'
+    csv_files_path = '/home/user4/Data/ufrj/ic/SP/2015/csv/'
+    columns_after_join = ['SP_ATOPROF', 'SP_CIDPRI', 'description']
 
-    df = sc.read_codes(path)
+    # get the sp files names 
+    sp_csv_files = sc.return_sp_files_on_path(csv_files_path)
 
-    print(df.head())
+    # lê os códigos CID relacionados a gravidez
+    preg_cid_codes = sc.read_codes(codes_file_path)
+
+    # empty dataframe that will contain the results
+    full_data = None
+
+    for csv_file in sp_csv_files:
+        
+        full_file_path = csv_files_path + csv_file
+        matches = sc.verify_codes(full_file_path, preg_cid_codes)
+        
+        if  not full_data:
+            full_data = matches
+        else:
+            full_data.append(matches)
+    
+    if full_data:
+        print('Escrevendo dados em um arquivo csv')
+        full_data.to_csv('results/resultados.csv', sep=',')
--- src/search_cids.py
+++ src/search_cids.py
@@ -1,6 +1,8 @@
 import os
 from typing import List
 import pandas as pd
+import numpy as np
+from pandas.core.reshape.merge import merge
 
 
 
@@ -14,7 +16,7 @@ def read_codes(path: str) -> pd.DataFrame:
     return codes
 
 
-def read_sp_files(path: str) -> List[str]:
+def return_sp_files_on_path(path: str) -> List[str]:
     """ Returns a list of csv filenames """
 
     filenames: List[str] = os.listdir(path)
@@ -23,8 +25,25 @@ def read_sp_files(path: str) -> List[str]:
     return csv_filenames
 
 
-def verify_codes(sp: pd.Dataframe, codes: pd.DataFrame) -> List[str]:
+def verify_codes(sp_file_fullpath: str, codes: pd.DataFrame) -> pd.DataFrame:
     """ Will return the a list of the professional codes rows matches any of the codes """
-    # TODO create left join with codes, then will be left only the matches
+    
+    columns = ['SP_ATOPROF', 'SP_CIDPRI']
+    types = {columns[0]: 'int64', columns[1]: str}
 
-    # TODO return the list of matches
+    sp_df = pd.read_csv(sp_file_fullpath, usecols=columns, dtype=types)
+    print(f'{sp_df.shape[0]} linhas encontradas')
+
+    # make left join
+    merged = pd.merge(left=sp_df, right=codes, how='left', left_on=columns[1], right_on='code')
+    # drop Nan
+    merged.dropna(inplace=True)
+    # remove duplicates
+    merged.drop_duplicates(inplace=True)
+    # drop duplicated column
+    merged.drop('code', axis='columns', inplace=True)
+
+    print(merged.head())
+    print(f'{merged.shape[0]} após o join')
+
+    return merged
